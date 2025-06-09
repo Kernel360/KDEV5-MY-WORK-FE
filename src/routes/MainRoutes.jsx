8f87d46b57edab6c5c27a9072557215ee9eb1a42
Yeju-Kang <bbogaeme@gmail.com>
2025-06-09 17:44:08 +0900

    fix: 로그인 요청에 대해 토큰 재발급 요청 제외 처(#78)

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -1,5 +1,6 @@
 // src/routes/MainRoutes.jsx
 import React from "react";
+import { useSelector } from "react-redux";
 import { Routes, Route, Navigate } from "react-router-dom";
 import ProjectPage from "@/features/project/pages/ProjectPage";
 import ProjectDetailPage from "@/features/project/pages/ProjectDetailPage";
@@ -13,7 +14,9 @@ import MemberFormPage from "@/features/member/pages/MemberFormPage";
 import MemberDetailPage from "@/features/member/pages/MemberDetailPage";
 
 export default function MainRoutes() {
-  const isAuthenticated = !!localStorage.getItem("accessToken");
+  const isAuthenticated = useSelector(
+    (state) => Boolean(state.auth?.accessToken)
+  );
 
   return (
     <Routes>
@@ -28,8 +31,10 @@ export default function MainRoutes() {
         }
       />
 
+      {/* 로그인 페이지 */}
       <Route path="/login" element={<LoginPage />} />
 
+      {/* 인증이 필요한 경로들 */}
       <Route
         element={
           isAuthenticated ? <MainLayout /> : <Navigate to="/login" replace />
@@ -39,7 +44,7 @@ export default function MainRoutes() {
 
         {/* 프로젝트 상세 하위 라우트 */}
         <Route path="/projects/:id">
-          {/* 기본 진입시 tasks로 리다이렉트 */}
+          {/* 기본 인덱스는 tasks 탭으로 */}
           <Route index element={<Navigate to="tasks" replace />} />
           <Route path="management" element={<ProjectDetailPage />} />
           <Route path="tasks" element={<ProjectDetailPage />} />
@@ -57,6 +62,7 @@ export default function MainRoutes() {
         <Route path="/dev-companies/new" element={<DevCompanyFormPage />} />
       </Route>
 
+      {/* 그 외 모든 경로: 로그인 여부에 따라 리다이렉트 */}
       <Route
         path="*"
         element={
