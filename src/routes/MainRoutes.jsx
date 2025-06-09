46e6da61f442e3e9d35cf19f70718eb206c11000
leeesooha <suha630@naver.com>
2025-06-09 18:37:52 +0900

    feat : 고객사 페이지 라우팅 추가 (#70)

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -6,7 +6,9 @@ import ProjectPage from "@/features/project/pages/ProjectPage";
 import ProjectDetailPage from "@/features/project/pages/ProjectDetailPage";
 import ProjectFormPage from "@/features/project/pages/ProjectFormPage";
 import DevCompanyPage from "@/features/company/pages/DevCompanyPage";
+import ClientCompanyPage from "@/features/company/pages/ClientCompanyPage";
 import DevCompanyFormPage from "@/features/company/pages/DevCompanyFormPage";
+import ClientCompanyFormPage from "@/features/company/pages/ClientCompanyFormPage";
 import MainLayout from "@/layouts/MainLayout";
 import LoginPage from "@/features/auth/pages/LoginPage";
 import MemberPage from "@/features/member/pages/MemberPage";
@@ -55,6 +57,9 @@ export default function MainRoutes() {
 
         <Route path="/dev-companies" element={<DevCompanyPage />} />
         <Route path="/dev-companies/new" element={<DevCompanyFormPage />} />
+
+        <Route path="/client-companies" element={<ClientCompanyPage />} />
+        <Route path="/client-companies/new" element={<ClientCompanyFormPage />} />
       </Route>
 
       <Route
