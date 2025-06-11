d300a03ca68c4dd61ce15360917f3f5c2bfa8244
Yeju-Kang <bbogaeme@gmail.com>
2025-06-11 22:17:00 +0900

    refactor: 탭 메뉴 스타일 조정(#119)

--- src/features/project/management/pages/ProjectManagement.jsx
+++ src/features/project/management/pages/ProjectManagement.jsx
@@ -60,6 +60,7 @@ export default function ProjectManagement() {
         width: "100%",
         height: "100%",
         overflow: "hidden",
+        mt: 1
       }}
     >
       {visibleSections.map((sec, idx) => (
