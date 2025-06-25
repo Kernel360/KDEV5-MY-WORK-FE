aa58a869d790498c7a862154fb1b47736ae2898a
Yeju-Kang <bbogaeme@gmail.com>
2025-06-26 05:30:59 +0900

    feat:공용 AlertMessage 컴포넌트 추가(#241)

---
+++ src/components/common/alertMessage/AlertMessage.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Snackbar, Alert } from "@mui/material";
+
+export default function AlertMessage({
+  open,
+  onClose,
+  message,
+  severity = "info",
+}) {
+  return (
+    <Snackbar
+      open={open}
+      autoHideDuration={3000}
+      onClose={onClose}
+      anchorOrigin={{ vertical: "top", horizontal: "center" }}
+    >
+      <Alert
+        onClose={onClose}
+        severity={severity}
+        variant="filled"
+        sx={{
+          borderRadius: 2,
+          fontSize: "0.9rem",
+          px: 2,
+          py: 1,
+          bgcolor: (theme) => theme.palette.status?.[severity]?.bg || "",
+          color: (theme) => theme.palette.status?.[severity]?.main || "",
+          boxShadow: 1,
+        }}
+      >
+        {message}
+      </Alert>
+    </Snackbar>
+  );
+}
