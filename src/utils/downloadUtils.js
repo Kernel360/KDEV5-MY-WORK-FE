f7a9cbc21eecd399f38d0c5b44f582f9217043ce
leeesooha <suha630@naver.com>
2025-07-14 15:50:26 +0900

    feat : 게시글 조회시 모든 첨부 파일 다운로드하는 기능 제거  (#457) - 게시글 조회시 파일 다운로드를 클릭해야 첨부파일 다운로드가 되게 수정하였습니다.

--- src/utils/downloadUtils.js
+++ src/utils/downloadUtils.js
@@ -1,10 +1,12 @@
 export async function downloadAttachment(attachment, getPresignedUrlFn) {
   try {
+    // 1. GET /api/posts/attachment/download-url - 다운로드 URL 발급
     const response = await getPresignedUrlFn(attachment.id);
 
     if (response.data.result === "SUCCESS") {
       const downloadUrl = response.data.data.downloadUrl;
 
+      // 2. GET {downloadUrl} - 실제 파일 다운로드
       const fileResponse = await fetch(downloadUrl);
       if (!fileResponse.ok) throw new Error("파일 다운로드 실패");
 
